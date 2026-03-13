9bfbd338a4c58487fcafab26fec5fa56bbd62b5e
Allen Liao <wcliaw610@gmail.com>
2026-03-12 19:22:11 -0500

    disko and setup notes for vm

---
+++ disko.nix
@@ -0,0 +1,45 @@
+{
+  disko.devices = {
+    disk.main = {
+      type = "disk";
+      device = "/dev/vda";
+      content = {
+        type = "gpt";
+        partitions = {
+          # BIOS boot partition
+          boot = {
+            size = "1M";
+            type = "EF02"; # BIOS boot
+            priority = 1;
+          };
+          # Main btrfs partition
+          root = {
+            size = "100%";
+            content = {
+              type = "btrfs";
+              extraArgs = [ "-f" ]; # force overwrite
+              subvolumes = {
+                "@" = {
+                  mountpoint = "/";
+                  mountOptions = [ "compress=zstd" "noatime" ]; 
+                };
+                "@home" = {
+                  mountpoint = "/home";
+                  mountOptions = [ "compress=zstd" "noatime" ];
+                };
+                "@nix" = {
+                  mountpoint = "/nix";
+                  mountOptions = [ "compress=zstd" "noatime" ];
+                };
+                "@snapshots" = {
+                  mountpoint = "/.snapshots";
+                  mountOptions = [ "compress=zstd" "noatime" ];
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
