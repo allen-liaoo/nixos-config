5a5beca160ee7bfdc2701f92f05a211163f88665
Allen Liao (from VM) <wcliaw610@gmail.com>
2026-03-18 04:53:25 +0000

    prevent zellij auto start

--- home/modules/term/zellij.nix
+++ home/modules/term/zellij.nix
@@ -3,9 +3,10 @@
 {
   programs.zellij = {
     enable = true;
-    enableBashIntegration = true;
-    enableFishIntegration = true;
-    enableZshIntegration = true;
+    # below auto-starts zellij
+    #enableBashIntegration = true;
+    #enableFishIntegration = true;
+    #enableZshIntegration = true;
   };
 
   xdg.configFile."zellij/config.kdl".source = config.lib.file.mkOutOfStoreSymlink (aln.lib.outOfStoreRelToRoot config.home.homeDirectory ./zellij_config.kdl);
