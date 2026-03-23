8e9e8d70970bad625e8b76989f74f7f4995bd98d
Allen Liao <wcliaw610@gmail.com>
2026-03-23 11:44:13 -0500

    try to fix persistence of ssh host keys

--- host/_modules/sops.nix
+++ host/_modules/sops.nix
@@ -5,8 +5,10 @@
     defaultSopsFile = aln.ctx.host.sopsFilePath;
     # Host SSH Keys are used to decrypt secrets
     # Each host is guaranteed to have a host key generated when first booted up (see host sshd config)
-    age.sshKeyPaths = [
-      # USE STRINGS, DONT NOT USE PATHS (otherwise it gets written to nix store unencrypted)
+    # USE STRINGS, DONT NOT USE PATHS (otherwise it gets written to nix store unencrypted)
+    age.sshKeyPaths = if aln.ctx.host.hasTags [ "impermanent" ] then [
+      "/persist/etc/ssh/ssh_host_ed25519_key" 
+    ] else [
       "/etc/ssh/ssh_host_ed25519_key" 
     ];
 
