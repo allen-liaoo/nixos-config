6893aef45501b5d8fe2c0db4f13c2426e4be5b92
Allen Liao <wcliaw610@gmail.com>
2026-03-24 21:34:15 -0500

    barrybenson firewall fully setup

--- host/barrybenson/network.nix
+++ host/barrybenson/network.nix
@@ -3,7 +3,8 @@
 let
   eth_intf = "enp1s0";
   wg_intf = "wg_vps";
-  wg_mark = "51820";
+  wg_mark = 51820;
+  wg_table = 1000;
 in
 {
   networking.useNetworkd = true;
@@ -36,16 +37,17 @@ in
               flags interval;
               elements = { 10.0.0.0/24, 10.0.10.0/24 }
           }
+          chain wg_input {
+            tcp dport { 8080, 8443 } accept
+          }
           chain input {
             type filter hook input priority 0; policy drop;
             ct state { established, related } accept
             iif "lo" accept
             ip protocol icmp limit rate 10/second accept
             ip6 nexthdr icmpv6 limit rate 10/second accept
-
             tcp dport 22 accept # ssh
-
-            iifname "${wg_intf}" ip saddr @WG_SUBNETS accept
+            iifname "${wg_intf}" ip saddr @WG_SUBNETS jump wg_input
           }
           chain forward {
             type filter hook forward priority 0; policy drop;
@@ -68,21 +70,27 @@ in
     };
   };
 
-  # wireguard tunnel to vps
-  systemd.network.networks."20-${wg_intf}" = {
-    matchConfig.Name = "${wg_intf}";
-    address = [ "10.0.0.2/24" ]; # this server's wg ip
-    #routingPolicyRules = [{
-      #FirewallMark = ${wg_mark};
-      #Table = ${wg_mark};
-      #Priority = 100;
-    #}];
-    #routes = [{
-      #Destination = "0.0.0.0/0";
-      #Table = ${wg_mark};
-    #}];
-  };
+  # Wireguard
+  # Goal: For any outgoing packet in reply to incoming packets from wg tunnel,
+  # route back through wg tunnel. Otherwise route through open internet
+  # To achieve this, we mark the new connections established from tunnel
+  # Then conditionally mark outgoing packets if the connection is marked
+  # The outgoing packet mark determines the routing table to lookup, which determines the route
+  networking.nftables.tables.wg_mark = {
+    family = "inet";
+    content = ''
+      chain prerouting {
+        type filter hook prerouting priority -150; policy accept;
+        iifname "wg0" ct state new ct mark set ${toString wg_mark} # mark conn if conn new
+      }
 
+      chain output {
+        type route hook output priority -150; policy accept;
+        ct mark ${toString wg_mark} meta mark set ${toString wg_mark} # mark outgoing packet based on conn mark
+      }
+    '';
+  };
+  # Wireguard conf
   systemd.network.netdevs."20-${wg_intf}" = {
     netdevConfig = {
       Name = wg_intf;
@@ -90,15 +98,31 @@ in
     };
     wireguardConfig = {
       PrivateKeyFile = config.sops.secrets.wg_privkey.path;
+      RouteTable = "off"; # dont configure route for outgoing packets destined for AllowedIPs
+      # Don't configure FirewallMark (mark out outgoing packets) 
     };
     wireguardPeers = [{
+      # vps
       PublicKey = "CQvf4nOExaVkaiWpsxx0OctRU4N51xRYdKUoKteegQk=";
-      Endpoint = "74.208.158.11:${wg_mark}";
-      AllowedIPs = [ "10.0.0.1/32" ];
+      Endpoint = "74.208.158.11:51820";
+      AllowedIPs = [ "0.0.0.0/0" ]; # this wouldve route everything through tunnel if RouteTable is not off
       PersistentKeepalive = 25;
     }];
   };
-
+  # Wireguard network routing
+  systemd.network.networks."20-${wg_intf}" = {
+    matchConfig.Name = "${wg_intf}";
+    address = [ "10.0.0.2/24" ]; # this server's wg ip
+    routingPolicyRules = [{
+      FirewallMark = wg_mark; # for any packet marked
+      Table = wg_table; # use this table
+      Priority = 100; # low priority = high number
+    }];
+    routes = [{
+      Table = wg_table; # route traffic of this table
+      Destination = "10.0.0.1"; # to vps ip
+    }];
+  };
   environment.systemPackages = with pkgs; [ wireguard-tools ];
 
   sops.secrets.wg_privkey = {
