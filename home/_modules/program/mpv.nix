82a860af9cf4c13736a79dadb54c1b54e95e793b
Allen Liao <wcliaw610@gmail.com>
2026-04-26 22:43:12 -0500

    feat: mpv scripts

--- home/_modules/program/mpv.nix
+++ home/_modules/program/mpv.nix
@@ -1,7 +1,20 @@
- { ... }:
+{
+  pkgs,
+  ...
+}:
   
- {
-   programs.mpv = {
-     enable = true;
-   };
- }
+{
+  programs.mpv = {
+    enable = true;
+    scripts = with pkgs.mpvScripts; [
+      autosub
+      mpris
+    ];
+  };
+
+  # TODO: Wait for Tampermonkey support: https://github.com/Tampermonkey/tampermonkey/issues/2002#issuecomment-2000358691
+  # home.packages = [
+  #   pkgs.mpv-handler
+  # ];
+  # TODO: add mime types
+}
