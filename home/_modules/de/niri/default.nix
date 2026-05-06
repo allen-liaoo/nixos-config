a0374eec66f35d5319db74bf387a95ce69f746f7
Allen Liao <wcliaw610@gmail.com>
2026-05-06 00:09:54 -0500

    tweak: mpv start full screen

--- home/_modules/de/niri/default.nix
+++ home/_modules/de/niri/default.nix
@@ -19,6 +19,12 @@ let
       }
     }
   '';
+  mpvKdl = pkgs.writeText "mpv.kdl" ''
+    window-rule {
+      match app-id=r#"mpv"#
+      open-fullscreen true
+    }
+  '';
   vicinaeKdl = pkgs.writeText "vicinae.kdl" ''
     binds {
       Mod+X repeat=false { spawn "${config.services.vicinae.package}/bin/vicinae" "toggle"; }
@@ -61,18 +67,21 @@ in
     include "dms/wpblur.kdl"  // blurring wallpaper settings
     ''}
 
-    ${lib.optionalString (config.services.vicinae.enable) ''
-    include "${vicinaeKdl}"
-    ''}
-
     ${lib.optionalString (config.programs.alacritty.enable) ''
     include "${alacrittyKdl}"
+    ''}
+
+    ${lib.optionalString (config.programs.mpv.enable) ''
+    include "${mpvKdl}"
+    ''}
+
+    ${lib.optionalString (config.services.vicinae.enable) ''
+    include "${vicinaeKdl}"
     ''}
 
     ${with config.i18n.inputMethod; lib.optionalString (enable && (type == "fcitx5")) ''
     include "${fcitx5Kdl}"
     ''}
-
   '';
 }
 # wrap in nixGL to fix OpenGL under nix in non-Nixos systems
