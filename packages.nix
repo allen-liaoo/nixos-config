a0bfcd1bb79499fc51b19cfc49315715089a0cef
Allen Liao <wcliaw610@gmail.com>
2026-05-10 15:48:36 -0500

    refactor: reoganize pkgs folder

--- packages.nix
+++ packages.nix
@@ -3,12 +3,12 @@
 }:
 
 {
-  typst-mcp = pkgs.python3Packages.callPackage ./pkgs/typst-mcp.nix rec {
+  typst-mcp = pkgs.python3Packages.callPackage ./pkgs/typst-mcp/typst-mcp.nix rec {
     typst = pkgs.typst;
-    typst-docs = pkgs.callPackage ./pkgs/typst-docs.nix {
+    typst-docs = pkgs.callPackage ./pkgs/typst-mcp/typst-docs.nix {
       typst = typst;
     };
-    mcp = pkgs.python3Packages.callPackage ./pkgs/mcp.nix { };
+    mcp = pkgs.python3Packages.callPackage ./pkgs/typst-mcp/mcp.nix { };
   };
   wavefox = pkgs.callPackage ./pkgs/wavefox.nix { };
 }
