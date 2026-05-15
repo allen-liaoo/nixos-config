21dc6646b0b00cba53d959abcedc9bd82ae30fc7
Allen Liao <wcliaw610@gmail.com>
2026-05-14 22:52:32 -0500

    feat(mc/summer26): add impermanecne and jvm opts

--- host/barrybenson/selfhosted/minecraft/default.nix
+++ host/barrybenson/selfhosted/minecraft/default.nix
@@ -17,6 +17,14 @@
     enable = true;
     eula = true;
     openFirewall = false;
+    managementSystem = {
+      tmux.enable = false;
+      systemd-socket.enable = true;
+    };
   };
+
+  aln.impermanence.dirs = [
+    "/srv/minecraft"
+  ];
 }
 # For declaring mods, see scripts/modrinth_prefetch.sh
--- host/barrybenson/selfhosted/minecraft/summer26/default.nix
+++ host/barrybenson/selfhosted/minecraft/summer26/default.nix
@@ -7,19 +7,21 @@
   services.minecraft-servers = {
     servers.summer26 = {
       enable = true;
+      package = pkgs.neoforgeServers.neoforge-1_21_1-21_1_228; # 229 too new
+
       autoStart = true;
       enableReload = true;
       serverProperties = {
         server-port = 25565;
-        white-list = false;
+        white-list = true;
         enable-rcon = true;
         difficulty = 3;
       };
+      jvmOpts = [
+        "-Xmx10G" "-Xms1G"
+      ];
 
-      package = pkgs.neoforgeServers.neoforge-1_21_1-21_1_228; # 229
-
-      #whitelist = import ../players.nix;
-
+      whitelist = import ../players.nix;
       symlinks = {
         mods = pkgs.linkFarmFromDrvs "mods" (
           map pkgs.fetchurl (builtins.attrValues (import ./mods.nix))
