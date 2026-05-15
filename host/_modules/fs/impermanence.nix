0b749bd9be721fa026c956bd85ca5ecd875fb4f4
Allen Liao <wcliaw610@gmail.com>
2026-05-14 22:52:08 -0500

    feat: add impermanence options

--- host/_modules/fs/impermanence.nix
+++ host/_modules/fs/impermanence.nix
@@ -2,6 +2,7 @@
   lib,
   inputs,
   ctx,
+  config,
   ...
 }:
 
@@ -19,129 +20,145 @@
 let
   disk_root = "btrfsroot";
   root_subvol = "@";
-in
-lib.optionalAttrs (ctx.host.hasTag.impermanent) {
-  imports = [
+  enable = ctx.host.hasTag.impermanent;
+  persistOpts = {
+    dirs = lib.mkOption {
+      type = with lib.types; listOf str;
+      default = [];
+    };
+    files = lib.mkOption {
+      type = with lib.types; listOf str;
+      default = [];
+    };
+  };
+in {
+  options = {
+    aln.impermanence = persistOpts;
+  };
+
+  imports = lib.optionals enable [
     inputs.impermanence.nixosModules.impermanence
   ];
 
-  # below paths shoud only contain data that are under root subvolume
-  # NOTE: do not persist both a file and its parent directory
-  environment.persistence."/persist" = {
-    enable = true;
-    hideMounts = true;
-    directories = [
-      "/var/lib/nixos" # nixos state
+  config = lib.optionalAttrs enable {
+    # below paths shoud only contain data that are under root subvolume
+    # NOTE: do not persist both a file and its parent directory
+    environment.persistence."/persist" = {
+      enable = true;
+      hideMounts = true;
+      directories = config.aln.impermanence.dirs ++ [
+        "/var/lib/nixos" # nixos state
 
-      "/var/log" # system logs
-      "/var/lib/systemd/coredump" # crash dumps
-      "/var/lib/systemd/timers"
-    ]
-    ++ lib.optionals (ctx.host.is.server) [
-      "/var/lib/systemd/network"
-      #"/var/lib/containers" # since this is on separate subvolume, no need to persist it as it wont be wiped
-    ]
-    ++ lib.optionals (!ctx.host.is.server) [
-      "/var/lib/bluetooth"
-      "/etc/NetworkManager/system-connections"
-      "/var/lib/cups" # printer
-    ]
-    ++
-      # For each user NOT using impermanence: persist their home directory
-      lib.concatMap (
-        user:
-        lib.optionals true [
-          # (!user.hasTags [ "impermanent" ]) [
-          {
-            directory = "/home/${user.name}";
-            user = user.name;
-            mode = "0700";
-          }
-        ]
-      ) ctx.host.users;
+        "/var/log" # system logs
+        "/var/lib/systemd/coredump" # crash dumps
+        "/var/lib/systemd/timers"
+      ]
+      ++ lib.optionals (ctx.host.is.server) [
+        "/var/lib/systemd/network"
+        #"/var/lib/containers" # since this is on separate subvolume, no need to persist it as it wont be wiped
+      ]
+      ++ lib.optionals (!ctx.host.is.server) [
+        "/var/lib/bluetooth"
+        "/etc/NetworkManager/system-connections"
+        "/var/lib/cups" # printer
+      ]
+      ++
+        # For each user NOT using impermanence: persist their home directory
+        lib.concatMap (
+          user:
+          lib.optionals true [
+            # (!user.hasTags [ "impermanent" ]) [
+            {
+              directory = "/home/${user.name}";
+              user = user.name;
+              mode = "0700";
+            }
+          ]
+        ) ctx.host.users;
 
-    files = [
-      "/etc/machine-id" # stable machine identity
+      files = config.aln.impermanence.files ++ [
+        "/etc/machine-id" # stable machine identity
 
-      # these are the only files necessary to persist on initial install,
-      # the rest of the configs can be generated from the config with these keys
-      "/etc/ssh/ssh_host_ed25519_key"
-      "/etc/ssh/ssh_host_ed25519_key.pub"
-    ];
-
-    # For each user using impermanence: persist specific directories
-    # TODO: Not working!! Can't get home-manager state to persist for some reason,
-    # and it's not trivial to widen or narrow persistence
-    # also may need systemd service to auto activate home manager; not desirable
-    #users = lib.mergeAttrsList (map (user: {
-    #${user.name} = lib.optionalAttrs (user.hasTags [ "impermanent" ]) {
-    #directories = [
-    #{ directory = ".ssh"; mode = "0700"; }
-    #{ directory = ".config/sops"; mode = "0700"; }
-    #"nix-config"
-
-    ## Supposedly these saves home manger state
-    #".local/state/nix"
-    #".local/state/home-manager"
-    #];
-    #files = [];
-    #};
-    #}) ctx.host.users);
-  };
-
-  # Reset root subvolume on boot
-  # this is better than a initrd script because it allows us to specify service orders
-  # https://www.notashelf.dev/posts/impermanence
-  # https://www.mankier.com/7/bootup
-  boot.initrd.supportedFilesystems = [ "btrfs" ];
-  boot.initrd.systemd = {
-    enable = true;
-    services.impermanence = {
-      description = "Wipe BTRFS root subvolume on boot";
-      wantedBy = [ "initrd.target" ]; # technically unnecessary as this is after sysroot.mount
-      before = [
-        "sysroot.mount" # before /sysroot is mounted so we can wipe
-
-        # IMPORTANT to make impermanence work with sops-nix without forcing it to read secrets/keys from /persist
-        "sops-install-secrets.service"
-        "sops-install-secrets-for-users.service" # need services.userborn.enable = true (see users.nix)
+        # these are the only files necessary to persist on initial install,
+        # the rest of the configs can be generated from the config with these keys
+        "/etc/ssh/ssh_host_ed25519_key"
+        "/etc/ssh/ssh_host_ed25519_key.pub"
       ];
-      # need btrfs partition to be mounted
-      after = [
-        "dev-disk-by\\x2dlabel-${disk_root}.device"
-        #"systemd-cryptsetup@end.service" # luks
-      ];
-      requires = [ "dev-disk-by\\x2dlabel-${disk_root}.device" ];
-      unitConfig.DefaultDependencies = "no";
-      serviceConfig.Type = "oneshot";
-      script = ''
-        # Mount the raw btrfs top-level somewhere temporary
-        mkdir /btrfs_tmp
-        mount /dev/disk/by-label/${disk_root} /btrfs_tmp
 
-        # If a previous root subvolume exists, archive it with a timestamp
-        if [[ -e /btrfs_tmp/${root_subvol} ]]; then
-          mkdir -p /btrfs_tmp/old_roots
-          timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/${root_subvol})" "+%Y-%m-%-d_%H:%M:%S")
-          mv /btrfs_tmp/${root_subvol} "/btrfs_tmp/old_roots/$timestamp"
-        fi
+      # For each user using impermanence: persist specific directories
+      # TODO: Not working!! Can't get home-manager state to persist for some reason,
+      # and it's not trivial to widen or narrow persistence
+      # also may need systemd service to auto activate home manager; not desirable
+      #users = lib.mergeAttrsList (map (user: {
+      #${user.name} = lib.optionalAttrs (user.hasTags [ "impermanent" ]) {
+      #directories = [
+      #{ directory = ".ssh"; mode = "0700"; }
+      #{ directory = ".config/sops"; mode = "0700"; }
+      #"nix-config"
 
-        # Delete archived roots older than 14 days
-        for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +14); do
-          btrfs subvolume delete --recursive "$i"
-        done
-
-        # Create a fresh empty root subvolume for this boot
-        btrfs subvolume create /btrfs_tmp/${root_subvol}
-        umount /btrfs_tmp
-        # Now NixOS mounts this fresh /root subvolume as /
-      '';
+      ## Supposedly these saves home manger state
+      #".local/state/nix"
+      #".local/state/home-manager"
+      #];
+      #files = [];
+      #};
+      #}) ctx.host.users);
     };
-  };
 
-  # Need to mark all btrfs subvolumes who are source or target of persistence bind mounts as neededForBoot
-  # Otherwiswe there will be no subvolume to bind mount from/to
-  fileSystems."/persist".neededForBoot = true;
+    # Reset root subvolume on boot
+    # this is better than a initrd script because it allows us to specify service orders
+    # https://www.notashelf.dev/posts/impermanence
+    # https://www.mankier.com/7/bootup
+    boot.initrd.supportedFilesystems = [ "btrfs" ];
+    boot.initrd.systemd = {
+      enable = true;
+      services.impermanence = {
+        description = "Wipe BTRFS root subvolume on boot";
+        wantedBy = [ "initrd.target" ]; # technically unnecessary as this is after sysroot.mount
+        before = [
+          "sysroot.mount" # before /sysroot is mounted so we can wipe
+
+          # IMPORTANT to make impermanence work with sops-nix without forcing it to read secrets/keys from /persist
+          "sops-install-secrets.service"
+          "sops-install-secrets-for-users.service" # need services.userborn.enable = true (see users.nix)
+        ];
+        # need btrfs partition to be mounted
+        after = [
+          "dev-disk-by\\x2dlabel-${disk_root}.device"
+          #"systemd-cryptsetup@end.service" # luks
+        ];
+        requires = [ "dev-disk-by\\x2dlabel-${disk_root}.device" ];
+        unitConfig.DefaultDependencies = "no";
+        serviceConfig.Type = "oneshot";
+        script = ''
+          # Mount the raw btrfs top-level somewhere temporary
+          mkdir /btrfs_tmp
+          mount /dev/disk/by-label/${disk_root} /btrfs_tmp
+
+          # If a previous root subvolume exists, archive it with a timestamp
+          if [[ -e /btrfs_tmp/${root_subvol} ]]; then
+            mkdir -p /btrfs_tmp/old_roots
+            timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/${root_subvol})" "+%Y-%m-%-d_%H:%M:%S")
+            mv /btrfs_tmp/${root_subvol} "/btrfs_tmp/old_roots/$timestamp"
+          fi
+
+          # Delete archived roots older than 14 days
+          for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +14); do
+            btrfs subvolume delete --recursive "$i"
+          done
+
+          # Create a fresh empty root subvolume for this boot
+          btrfs subvolume create /btrfs_tmp/${root_subvol}
+          umount /btrfs_tmp
+          # Now NixOS mounts this fresh /root subvolume as /
+        '';
+      };
+    };
+
+    # Need to mark all btrfs subvolumes who are source or target of persistence bind mounts as neededForBoot
+    # Otherwiswe there will be no subvolume to bind mount from/to
+    fileSystems."/persist".neededForBoot = true;
+  };
 }
 
 # Below is old code that is necessary if one uses a initrd script (initrd.postResumeCommands)
